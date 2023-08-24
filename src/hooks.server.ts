7753d5ced4dee509c706177d4a448ad3f2b215ef
aakash_goplani_nagarro <aakash.goplani@nagarro.com>
2023-08-24 21:47:08 +0530

    code cleanup

--- src/hooks.server.ts
+++
@@ -1,7 +0,0 @@
-import type { Handle } from "@sveltejs/kit";
-//import { importShim } from 'es-module-shims';
-
-export const handle = (({ event, resolve }) => {
-  event.locals.window = {};
-  return resolve(event);
-}) satisfies Handle;
